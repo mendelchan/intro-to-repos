e963059ba96b2c552b5132b43fdcdf46908339cf
chan.mendelsohn.neil@gmail.com <chan.mendelsohn.neil@gmail.com>
2021-12-18 06:12:59 +0000

    Blank Cell Repo

--- my_lib/my_funcs.py
+++ my_lib/my_funcs.py
@@ -4,10 +4,18 @@
 # MAGIC ## This is a Python Notebook with Functions Inside 
 # MAGIC ### ```This is a Databricks notebook Source```
 
+# COMMAND ----------
 
 def convertFtoC(unitCol, tempCol):
     from pyspark.sql.functions import when, col
     return when(col(unitCol) == "F", (col(tempCol) - 32) * (5/9)).otherwise(col(tempCol)).alias("temp_celcius")
+
+
+# COMMAND ----------
+
+# THIS IS A BLANK CELL
+
+# COMMAND ----------
 
 
 def roundedTemp(unitCol, tempCol):
